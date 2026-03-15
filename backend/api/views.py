0bfdb080331ec04dd6691162b6b39a7623076965
chauh_harsh <chauhanharsh226@gmail.com>
2026-03-15 23:16:02 +0530

    Initiated the models

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
+@api_view(['Get'])
+def home(request):
+    return Response("Hello This is home page.")
+
 @api_view(['Get'])
 def users(request):
     data = [
